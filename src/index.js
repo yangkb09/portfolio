cafb15058b8c9cb0976cb9386c8d7bac99d0ea5b
Katarina Yang <yangkb09@gmail.com>
2021-07-24 17:57:24 -0400

    chore: delete app.js filler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import Intro from './Intro';
 import About from './About';
 import Projects from './Projects';
@@ -20,7 +19,6 @@ ReactDOM.render(
     <Experience />
     <Footer />
     <Sidebar />
-    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
