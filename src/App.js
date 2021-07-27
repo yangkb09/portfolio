c23c791830c05784390b466a2858a2786f2f9272
Katarina Yang <yangkb09@gmail.com>
2021-07-27 17:21:09 -0400

    feat & style: build content and style intro component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import TechStack from './TechStack';
 import Experience from './Experience';
 import Footer from './Footer';
 import Sidebar from './Sidebar';
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 const Container = styled.div`
   background-color: #2c2f33;
   color: white;
   font: Poppins;
   min-height: 100vh;
+  font-size: 16px;
+  padding: 10rem;
   /* display: flex; */
 `
 
--- src/Intro.js
+++ src/Intro.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 const ImportContainer = styled.div`
-  color: purple;
+  font-size: 5rem;
+  font-weight: bold;
+  text-shadow: 4px 4px #7289da;
+  align-items: center;
+
+`
+const Image = styled.img`
+  border-radius: 50%;
+  max-width: 25%;
+  box-shadow: 4px 4px white;
+`
+
+const Header = styled.div`
+  /* float: left; */
+`
+
+const Greeting = styled.span`
+  align-items: center;
 `
 
 export const Intro = () => {
   return (
-      <ImportContainer>Placeholder for Intro</ImportContainer>
+      <ImportContainer>
+        <Header>
+          <Image
+            src="/katpic.jpg"
+            alt="Kat looking directly at camera in black t-shirt and gold necklace"
+          />
+          <Greeting>
+            I'm Kat, nice to meet you!
+          </Greeting>
+        </Header>
+      </ImportContainer>
   )
 }
 
