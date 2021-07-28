e7fbe7a5053841331bb4d4690d4eeb8b07c32648
Katarina Yang <yangkb09@gmail.com>
2021-07-28 11:32:48 -0400

    style: add global styles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+body {
+  padding: 10rem;
+  background-color: #2c2f33;
+  color: white;
+  font: Poppins;
+  font-size: 16px;
+}
+`
+export default GlobalStyle;
